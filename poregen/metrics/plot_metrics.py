83dbf8d87df0f149901de3adda3dee33ee1255bd
Danilo Naiff <danilonaiff1992@gmail.com>
2024-10-11 12:41:44 -0300

    Added plot_conditional_metrics function This is for testing whether the generated samples are compatible with the passed condition.

--- poregen/metrics/plot_metrics.py
+++ poregen/metrics/plot_metrics.py
@@ -11,11 +11,13 @@ from scipy import stats, interpolate
 def plot_unconditional_metrics(datapath, voxel_size_um=None):
     # voxel_size in um
     cfg_path = f"{datapath}/config.yaml"
+
+    # TODO: Simplify this loop. It should begin with if voxel_size_um is None
     try:
         with open(cfg_path, "r") as f:
             cfg = yaml.safe_load(f)
     except FileNotFoundError:
-        assert voxel_size_um is not None, "voxel_size must be provided if config.yaml is not found"
+        assert voxel_size_um is not None, "voxel_size_um must be provided if config.yaml is not found"
     if voxel_size_um is None:
         try:
             voxel_size_um = cfg['data']['voxel_size_um']
@@ -131,6 +133,147 @@ def plot_unconditional_metrics(datapath, voxel_size_um=None):
     fig4.savefig(savefolder / "psd_comparison.png")
 
 
+def plot_conditional_metrics(datapath, voxel_size_um=None):
+    # TODO: Break this function into smaller functions!
+
+    # voxel_size in um
+    cfg_path = f"{datapath}/config.yaml"
+
+    # TODO: Simplify this loop. It should begin with if voxel_size_um is None
+    try:
+        with open(cfg_path, "r") as f:
+            cfg = yaml.safe_load(f)
+    except FileNotFoundError:
+        assert voxel_size_um is not None, "voxel_size_um must be provided if config.yaml is not found"
+    if voxel_size_um is None:
+        try:
+            voxel_size_um = cfg['data']['voxel_size_um']
+        except KeyError:
+            raise ValueError("voxel_size_um must be provided if config.yaml is not found")
+
+    generated_stats_path = f"{datapath}/generated_stats.json"
+    x_cond_stats_path = f"{datapath}/xcond_stats.json"
+
+    with open(generated_stats_path, "r") as f:
+        generated_stats = json.load(f)
+
+    with open(x_cond_stats_path, "r") as f:
+        x_cond_stats = json.load(f)
+
+    conditions = cfg['data']['feature_extractor']
+    fig1 = None
+    fig2 = None
+    fig3 = None
+    fig4 = None
+
+    if 'porosity' in conditions:
+        gen, _ = extract_property(generated_stats, 'porosity')
+        cond = x_cond_stats['porosity']
+        gen = gen.flatten()
+        cond = cond[0]
+
+        fig1, ax = plt.subplots(1, 1, figsize=(6, 6))
+        min_value = min(np.min(gen), cond)
+        max_value = max(np.max(gen), cond)
+        bins = np.linspace(min_value, max_value, 20)
+
+        ax.hist(gen, bins=bins, density=True, color='red', alpha=0.5, label='Generated')
+        ax.vlines(cond, 0, 10, color='black', label='Condition')
+
+        ax.set_xlabel(r"$\phi$")
+        ax.set_ylabel("Density")
+        ax.set_title('Porosity')
+        ax.legend()
+        fig1.tight_layout()
+
+    if 'porosimetry_from_voxel' in conditions:
+
+        generated_psd_pdf, _ = extract_property(generated_stats, 'psd_pdf')
+        generated_psd_cdf, _ = extract_property(generated_stats, 'psd_cdf')
+        generated_psd_centers, _ = extract_property(generated_stats, 'psd_centers')
+
+        x_cond_psd_pdf = x_cond_stats['psd_pdf']
+        x_cond_psd_cdf = x_cond_stats['psd_cdf']
+        x_cond_psd_centers = x_cond_stats['psd_centers']
+        x_cond_psd_pdf = np.array(x_cond_psd_pdf)
+        x_cond_psd_centers = np.array(x_cond_psd_centers)
+
+        # The centers have units of um
+        generated_psd_centers *= voxel_size_um
+        x_cond_psd_centers *= voxel_size_um
+
+        # Plot PDF
+        fig2, ax = plt.subplots(1, 1, figsize=(6, 6))
+        nplots = generated_psd_pdf.shape[1] - 1
+        for i in range(nplots):
+            ax.plot(generated_psd_centers[:, i], generated_psd_pdf[:, i], alpha=0.2, color='red')
+        ax.plot(generated_psd_centers[:, nplots], generated_psd_pdf[:, nplots], alpha=0.2, color='red',
+                label='Generated')
+
+        ax.plot(x_cond_psd_centers, x_cond_psd_pdf, color='black', label='Condition')
+
+        ax.set_xlabel(r'Pore Size $(\mu m)$')
+        ax.set_ylabel('Probability Density')
+        ax.set_title('Pore Size Distribution - PDF')
+        ax.legend()
+        ax.set_xscale('log')
+        fig2.tight_layout()
+
+        # Plot CDF
+        fig3, ax = plt.subplots(1, 1, figsize=(6, 6))
+        nplots = generated_psd_cdf.shape[1] - 1
+        for i in range(nplots):
+            ax.plot(generated_psd_centers[:, i], generated_psd_cdf[:, i], alpha=0.2, color='red')
+        ax.plot(generated_psd_centers[:, nplots], generated_psd_cdf[:, nplots], alpha=0.2, color='red',
+                label='Generated')
+
+        ax.plot(x_cond_psd_centers, x_cond_psd_cdf, color='black', label='Condition')
+
+        ax.set_xlabel(r'Pore Size $(\mu m)$')
+        ax.set_ylabel('Cumulative Probability')
+        ax.set_title('Pore Size Distribution - CDF')
+        ax.legend()
+        ax.set_xscale('log')
+        fig3.tight_layout()
+
+    if 'two_point_correlation_from_voxel' in conditions:
+        generated_tpc_dist, _ = extract_property(generated_stats, 'tpc_dist')
+        x_cond_tpc_dist, _ = extract_property(x_cond_stats, 'tpc_dist')
+
+        # tpc_dists have units of um
+        generated_tpc_dist *= voxel_size_um
+        x_cond_tpc_dist *= voxel_size_um
+
+        generated_tpc_prob, _ = extract_property(generated_stats, 'tpc_prob')
+        x_cond_tpc_prob, _ = extract_property(x_cond_stats, 'tpc_prob')
+
+        # x_generated = generated_tpc_dist.mean(axis=0)
+        # x_x_cond = x_cond_tpc_dist.mean(axis=0)
+
+        fig4, ax = plt.subplots(1, 1, figsize=(6, 6))
+        nplots = generated_tpc_dist.shape[1] - 1
+        for i in range(nplots):
+            ax.plot(generated_tpc_dist[:, i], generated_tpc_prob[:, i], alpha=0.2, color='red')
+        ax.plot(generated_tpc_dist[:, nplots], generated_tpc_prob[:, nplots], alpha=0.2, color='red', label='Generated')
+        ax.plot(x_cond_tpc_dist[:, 0], x_cond_tpc_prob[:, 0], color='black', label='Condition')
+
+        ax.set_xlabel(r"$r$ $(\mu m)$")
+        ax.set_ylabel(r"$(s_2 - \phi^2)/(\phi - \phi^2)$")
+        ax.legend()
+        ax.set_title("Comparison of two point correlation (TPC)")
+
+    savefolder = pathlib.Path(f"{datapath}/figures")
+    os.makedirs(savefolder, exist_ok=True)
+    if fig1 is not None:
+        fig1.savefig(savefolder / "porosity.png")
+    if fig2 is not None:
+        fig2.savefig(savefolder / "pore_size_distribution_pdf.png")
+    if fig3 is not None:
+        fig3.savefig(savefolder / "pore_size_distribution_cdf.png")
+    if fig4 is not None:
+        fig4.savefig(savefolder / "two_point_correlation.png")
+
+
 def extract_property(data, property_name):
     property_list = []
     input_list = []
@@ -141,9 +284,16 @@ def extract_property(data, property_name):
         if k == 'condition':
             continue
         if isinstance(v, dict) and property_name in v:
-            property_list.append(v[property_name])
+            value = np.array(v[property_name])
+            # Hack to check if the value is empty,
+            # or if there is any non-finite value
+            check = np.all(np.isfinite(value)) and len(value) > 0
+            if not check:
+                print("Empty or faulty value for key", k)
+                continue
+            property_list.append(np.array(v[property_name]))
             input_list.append(k)
-    property_list = np.array(property_list)
+    property_list = np.stack(property_list, axis=0)
     input_list = np.array(input_list)
     return property_list, input_list
 
